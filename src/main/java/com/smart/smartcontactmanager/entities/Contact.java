de81db4bb727105b56421dbacc933cf5c56f4ca6
dhawal-kamdar <dhawalkamdar7@gmail.com>
2022-02-12 11:41:26 +0530

    Added entities and dao classes

---
+++ src/main/java/com/smart/smartcontactmanager/entities/Contact.java
@@ -0,0 +1,101 @@
+package com.smart.smartcontactmanager.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int cId;
+    private String name;
+    private String nickName;
+    private String phone;
+    private String work;
+    private String email;
+    private String imageUrl;
+    @Column(length = 1000)
+    private String description;
+
+    @ManyToOne
+    private User user;
+
+    // Constructor
+    public Contact() {
+        super();
+    }
+
+    // Getters & Setters
+    public int getcId() {
+        return cId;
+    }
+
+    public void setcId(int cId) {
+        this.cId = cId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
