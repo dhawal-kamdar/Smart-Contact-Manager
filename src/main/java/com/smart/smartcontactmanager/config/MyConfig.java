e178a72e056b93776b27075eb68107f2d082b385
dhawal-kamdar <dhawalkamdar7@gmail.com>
2022-02-13 20:23:02 +0530

    Updated login system

--- src/main/java/com/smart/smartcontactmanager/config/MyConfig.java
+++ src/main/java/com/smart/smartcontactmanager/config/MyConfig.java
@@ -45,7 +45,11 @@ public class MyConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
                                 .antMatchers("/user/**").hasRole("USER")
                                 .antMatchers("/**").permitAll()
-                                .and().formLogin().loginPage("/signin")
+                                .and().formLogin()
+                                .loginPage("/signin")
+                                .loginProcessingUrl("/dologin")
+                                .failureUrl("/login-fail")
+                                .defaultSuccessUrl("/user/index")
                                 .and().csrf().disable();
     }
 
