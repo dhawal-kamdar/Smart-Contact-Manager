56383b88d94ce16339449f9d17dceba91483584c
dhawal-kamdar <dhawalkamdar7@gmail.com>
2022-02-15 21:59:18 +0530

    Added view contact page with pagination

--- src/main/java/com/smart/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/smart/smartcontactmanager/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.smart.smartcontactmanager.controller;
 
+import com.smart.smartcontactmanager.dao.ContactRepository;
 import com.smart.smartcontactmanager.dao.UserRepository;
 import com.smart.smartcontactmanager.entities.Contact;
 import com.smart.smartcontactmanager.entities.User;
 import com.smart.smartcontactmanager.helper.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -26,6 +30,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ContactRepository contactRepository;
 
     @ModelAttribute
     public void addCommonData(Model model, Principal principal) {
@@ -89,6 +96,23 @@ public class UserController {
         }
 
         return "normal/add-contact";
+    }
+
+    @GetMapping("/view-contacts/{page}")
+    public String viewContacts(@PathVariable("page") Integer page, Model model, Principal principal) {
+        model.addAttribute("title", "View Contacts");
+
+        String username = principal.getName();
+        User user = this.userRepository.getUserByUsername(username);
+
+        Pageable pageable = PageRequest.of(page, 5);
+        Page<Contact> contacts = this.contactRepository.findContactsByUser(user.getId(), pageable);
+
+        model.addAttribute("contacts", contacts);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", contacts.getTotalPages());
+
+        return "normal/view-contacts";
     }
     
 }
